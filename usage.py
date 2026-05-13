5d22a9da5fd827596ef71161d194242999010579
Thomas Bierhance <bierhance@gmx.de>
2026-05-13 22:57:40 +0200

    Use a Container component for the usage demo.

--- usage.py
+++ usage.py
@@ -7,20 +7,24 @@ app = dash.Dash()
 
 app.layout = dmc.MantineProvider(
     children=[
-        dmc.Alert(
-            "Welcome to Dash Mantine Components",
-            title="Hello!",
-            color="violet",
-        ),
+        dmc.Container(
+            children=[
+                dmc.Alert(
+                    "Welcome to Dash Mantine Components",
+                    title="Hello!",
+                    color="violet",
+                ),
 
-        dmc.Text("Limit max number of options that can be selected", mt=24),
-        dmc_custom.MaxSelectedItems(maxItems=2),
+                dmc.Text("Limit max number of options that can be selected", mt=24),
+                dmc_custom.MaxSelectedItems(maxItems=2),
 
-        dmc.Text("Select with option to create new options", mt=24),
-        dmc_custom.SelectCreatable(),
+                dmc.Text("Select with option to create new options", mt=24),
+                dmc_custom.SelectCreatable(),
 
-        dmc.Text("Limit max number of values that can be displayed", mt=24),
-        dmc_custom.MaxDisplayedItems(maxItems=2)
+                dmc.Text("Limit max number of values that can be displayed", mt=24),
+                dmc_custom.MaxDisplayedItems(maxItems=2)
+            ]
+        )
     ]
 )
 
